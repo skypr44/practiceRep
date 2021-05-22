90d838a9ec563ea13c4c0955181d54619955c469
skypr44 <69776790+skypr44@users.noreply.github.com>
2021-05-22 00:38:46 -0400

    adding

---
+++ Bagels/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbBagels = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCount = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tslDate = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblBagelCount = New System.Windows.Forms.Label()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmbBagels
+        '
+        Me.cmbBagels.FormattingEnabled = True
+        Me.cmbBagels.Location = New System.Drawing.Point(109, 19)
+        Me.cmbBagels.Name = "cmbBagels"
+        Me.cmbBagels.Size = New System.Drawing.Size(121, 21)
+        Me.cmbBagels.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(35, 55)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "&Add Bagel"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(145, 55)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(85, 23)
+        Me.btnRemove.TabIndex = 2
+        Me.btnRemove.Text = "&Remove Bagel"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Location = New System.Drawing.Point(35, 95)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "&Clear List"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCount
+        '
+        Me.btnCount.Location = New System.Drawing.Point(145, 95)
+        Me.btnCount.Name = "btnCount"
+        Me.btnCount.Size = New System.Drawing.Size(75, 23)
+        Me.btnCount.TabIndex = 4
+        Me.btnCount.Text = "&Begal Count"
+        Me.btnCount.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(90, 137)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.tslDate})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 174)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(284, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(68, 17)
+        Me.ToolStripStatusLabel1.Text = "Eric Skipper"
+        '
+        'tslDate
+        '
+        Me.tslDate.Name = "tslDate"
+        Me.tslDate.Size = New System.Drawing.Size(0, 17)
+        '
+        'lblBagelCount
+        '
+        Me.lblBagelCount.AutoSize = True
+        Me.lblBagelCount.Location = New System.Drawing.Point(51, 22)
+        Me.lblBagelCount.Name = "lblBagelCount"
+        Me.lblBagelCount.Size = New System.Drawing.Size(0, 13)
+        Me.lblBagelCount.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(284, 196)
+        Me.Controls.Add(Me.lblBagelCount)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCount)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.cmbBagels)
+        Me.Name = "Form1"
+        Me.Text = "Need-A-Bagel"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbBagels As ComboBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCount As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents tslDate As ToolStripStatusLabel
+    Friend WithEvents lblBagelCount As Label
+End Class
---
+++ Bagels/WindowsApplication2/Form1.vb
@@ -0,0 +1,94 @@
+﻿'Eric Skipper
+'Chapter 5, Bagel making
+
+Option Strict On
+Option Explicit On
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'set date
+        Dim thisDay As Date
+        thisDay = Today
+        tslDate.Text = thisDay.ToString("d")
+
+        'set initial four values
+        cmbBagels.Items.Add("Blueberry")
+        cmbBagels.Items.Add("Whole Wheat")
+        cmbBagels.Items.Add("Poppyseed")
+        cmbBagels.Items.Add("Everything")
+
+        'sort the list and keep it sorted
+        cmbBagels.Sorted = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'input
+        Dim newBagel As String
+        newBagel = cmbBagels.Text.ToString
+
+        'processing
+        Dim isBagelAlreadyHere As Boolean
+        isBagelAlreadyHere = False
+
+        If newBagel <> "" Then
+            For counter = 0 To cmbBagels.Items.Count - 1
+                cmbBagels.SelectedIndex = counter
+                If cmbBagels.SelectedItem.ToString.ToLower = newBagel.ToLower Then
+                    isBagelAlreadyHere = True
+                End If
+            Next
+            'output
+            If isBagelAlreadyHere Then
+                MessageBox.Show("This bagel type already exists, try a new type of bagel.", "Not valid input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                cmbBagels.Text = ""
+                cmbBagels.Focus()
+            Else
+                cmbBagels.Items.Add(newBagel)
+                lblBagelCount.Text = ""
+                cmbBagels.Text = ""
+            End If
+        Else
+            MessageBox.Show("No Bagel inputed, we need a new bagel...", "Not valid input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            cmbBagels.Focus()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'input
+        Dim messageResponse As DialogResult
+        messageResponse = MessageBox.Show("Are you sure you want to clear the list?", "Desicions....", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+        'processing
+        If messageResponse = DialogResult.Yes Then
+            'output
+            cmbBagels.Items.Clear()
+            cmbBagels.Text = ""
+            lblBagelCount.Text = ""
+            cmbBagels.Focus()
+        End If
+    End Sub
+
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        lblBagelCount.Text = cmbBagels.Items.Count.ToString
+        cmbBagels.Focus()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        If cmbBagels.Items.Count = 0 Then
+            MessageBox.Show("No begals to delete, maybe add some...?", "Deletion Requirements", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            cmbBagels.Focus()
+        Else
+            If cmbBagels.SelectedIndex = -1 Then
+                MessageBox.Show("Please select a bagel to delete from the list.", "Deletion Requirments", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                cmbBagels.Focus()
+            Else
+                cmbBagels.Items.RemoveAt(cmbBagels.SelectedIndex)
+                cmbBagels.Focus()
+                lblBagelCount.Text = ""
+            End If
+        End If
+    End Sub
+End Class
